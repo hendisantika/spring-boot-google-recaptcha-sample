743df4ef5e35b84400917c01ba70eec4c36cb11a
hendisantika <hendisantika@yahoo.co.id>
2023-11-11 17:07:55 +0700

    #1 - Init Starter Project ✏️📗:octocat:🐧🐳⬆

---
+++ src/main/java/com/hendisantika/recaptchav3/SpringBootGoogleRecaptchaSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.recaptchav3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGoogleRecaptchaSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGoogleRecaptchaSampleApplication.class, args);
+    }
+
+}
