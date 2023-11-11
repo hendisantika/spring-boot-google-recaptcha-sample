363ffcfd18291802a795dfbc66973865d3db44e1
hendisantika <hendisantika@yahoo.co.id>
2023-11-11 17:40:56 +0700

    #20 - Add LoginController ✏️📗:octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/recaptchav3/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.hendisantika.recaptchav3.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-google-recaptcha-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/23
+ * Time: 17:40
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+@Slf4j
+public class LoginController {
+}
