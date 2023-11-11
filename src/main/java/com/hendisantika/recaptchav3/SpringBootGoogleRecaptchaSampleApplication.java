d5468e75e0d05349dda341efba819c9efa9135ac
hendisantika <hendisantika@yahoo.co.id>
2023-11-11 17:25:56 +0700

    #10 - Add commandLineRunner ✏️📗:octocat:🐧🐳

--- src/main/java/com/hendisantika/recaptchav3/SpringBootGoogleRecaptchaSampleApplication.java
+++ src/main/java/com/hendisantika/recaptchav3/SpringBootGoogleRecaptchaSampleApplication.java
@@ -1,10 +1,17 @@
 package com.hendisantika.recaptchav3;
 
+import com.hendisantika.recaptchav3.entity.Role;
+import com.hendisantika.recaptchav3.entity.User;
+import com.hendisantika.recaptchav3.repository.RoleRepository;
+import com.hendisantika.recaptchav3.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootGoogleRecaptchaSampleApplication {
@@ -16,6 +23,37 @@ public class SpringBootGoogleRecaptchaSampleApplication {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(UserRepository appUserRepository,
+                                        RoleRepository roleRepository,
+                                        PasswordEncoder passwordEncoder) {
+        return args -> {
+            List<Role> roles = List.of(
+                    new Role("ROLE_USER"),
+                    new Role("ROLE_ADMIN")
+            );
+
+            roleRepository.saveAll(roles);
+
+            List<User> appUsers = List.of(
+                    new User("naruto", passwordEncoder.encode("1234")),
+                    new User("gojo", passwordEncoder.encode("1234"))
+            );
+            appUserRepository.saveAll(appUsers);
+
+            User naruto = appUserRepository.findAppUserByUserName("naruto");
+            Role roleForJohn = roleRepository.findRoleByRoleName("ROLE_USER");
+            naruto.getRoles().add(roleForJohn);
+            appUserRepository.save(naruto);
+
+
+            User gojo = appUserRepository.findAppUserByUserName("gojo");
+            Role roleForGojo = roleRepository.findRoleByRoleName("ROLE_ADMIN");
+            gojo.getRoles().add(roleForGojo);
+            appUserRepository.save(gojo);
+        };
     }
 
 }
