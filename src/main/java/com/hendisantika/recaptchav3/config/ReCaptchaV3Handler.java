c8cbc8fd0dd5d8e979dea843b8b3172900b137af
hendisantika <hendisantika@yahoo.co.id>
2023-11-11 17:35:38 +0700

    #14 - Add ReCaptchaV3Handler ✏️📗:octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/recaptchav3/config/ReCaptchaV3Handler.java
@@ -0,0 +1,17 @@
+package com.hendisantika.recaptchav3.config;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-google-recaptcha-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/23
+ * Time: 17:34
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReCaptchaV3Handler {
+    private final String secretKey = "6Lc4JNUbAAAAANKqiE4UXytmsQw35UcHkzAScS_o";
+    private final String serverAddress = "https://www.google.com/recaptcha/api/siteverify";
+
+}
