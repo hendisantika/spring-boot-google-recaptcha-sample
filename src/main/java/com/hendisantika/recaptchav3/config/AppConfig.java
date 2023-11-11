d907e8f5c5c7f95c0b422936d97862c7fd9a987e
hendisantika <hendisantika@yahoo.co.id>
2023-11-11 17:34:25 +0700

    #13 - Add configure in AppConfig ✏️📗:octocat:🐧🐳

--- src/main/java/com/hendisantika/recaptchav3/config/AppConfig.java
+++ src/main/java/com/hendisantika/recaptchav3/config/AppConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Created by IntelliJ IDEA.
@@ -41,11 +42,24 @@ public class AppConfig extends WebSecurityConfigurerAdapter {
         return daoAuthenticationProvider;
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Bean
+    public SecurityFilterChain configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors().disable();
+        http.
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/processLogin")
+                .defaultSuccessUrl("/success", true)
+                .permitAll();
     }
 }
