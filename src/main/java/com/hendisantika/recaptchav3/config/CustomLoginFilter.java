b70e0d01eb290b79a5953f7789aaba1f431160e7
hendisantika <hendisantika@yahoo.co.id>
2023-11-11 17:38:46 +0700

    #17 - Add attemptAuthentication in CustomLoginFilter ✏️📗:octocat:🐧🐳

--- src/main/java/com/hendisantika/recaptchav3/config/CustomLoginFilter.java
+++ src/main/java/com/hendisantika/recaptchav3/config/CustomLoginFilter.java
@@ -1,8 +1,14 @@
 package com.hendisantika.recaptchav3.config;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,5 +26,27 @@ public class CustomLoginFilter extends UsernamePasswordAuthenticationFilter {
         super.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher(loginURL, httpMethod));
     }
 
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String recaptchaFormResponse = request.getParameter("g-recaptcha-response");
 
+        log.info("Before processing authentication.......");
+
+        ReCaptchaV3Handler handler = new ReCaptchaV3Handler();
+
+        try {
+            float score = handler.verify(recaptchaFormResponse);
+            if (score < 0.5) {
+                request.getRequestDispatcher("otp_login").forward(request, response);
+            }
+        } catch (InvalidReCaptchaTokenException | ServletException | IOException e) {
+            try {
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+
+        return super.attemptAuthentication(request, response);
+    }
 }
